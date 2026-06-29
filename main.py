79cf93374b18b2f55a34e34f785e4481564284c9
SONU <sonukumarsah@SONUs-MacBook-Pro.local>
2026-06-29 19:33:18 +0530

    allow wildcard CORS origins

--- main.py
+++ main.py
@@ -23,20 +23,10 @@ app = FastAPI(
 )
 
 # CORS configurations
-FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        FRONTEND_URL,
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:5175",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
